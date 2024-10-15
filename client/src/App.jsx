b33029dae4c4521f931cf04345949900567af577
AbhishekArrawatia <arrawatia30061997@gmail.com>
2024-10-15 23:26:38 +0530

    adding sign up page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Signin from "./pages/Signin";
-import Signout from "./pages/Signout";
+import Signup from "./pages/Signup";
 import About from "./pages/About";
 import Profile from "./pages/Profile";
 import Header from "./components/Header";
@@ -13,7 +13,7 @@ const App = () => {
       <Routes>
         <Route path="/" exact element={<Home />} />
         <Route path="/sign-in" element={<Signin />} />
-        <Route path="/sign-out" element={<Signout />} />
+        <Route path="/sign-up" element={<Signup />} />
         <Route path="/About" element={<About />} />
         <Route path="/Profile" element={<Profile />} />
       </Routes>
